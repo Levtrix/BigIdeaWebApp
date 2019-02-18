556c01185de66e96049a1c9db2840b919c0b88cf
Levtrix <m.k.a.janssen@gmail.com>
2019-02-18 08:54:17 +0100

    Added tests

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,18 @@
+import { browser, by, element } from 'protractor';
+import { AppPage } from './app.po';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should have a title', function() {
+    browser.get('http://localhost:4200');
+    browser.waitForAngularEnabled(false);
+
+    expect(browser.getTitle()).toEqual('Koffie App');
+  });
+
+});
